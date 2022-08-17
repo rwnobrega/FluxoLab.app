4e98c293206297d14cb2813922d6b750528654f3
Roberto Nobrega <rwnobrega@gmail.com>
2022-08-17 12:20:32 -0300

    Improve handle drag-and-drop

--- src/fluxolab/components/Flow/MyHandle.tsx
+++ src/fluxolab/components/Flow/MyHandle.tsx
@@ -14,33 +14,38 @@ type Props = HandleProps & {
 export default function ({ id, type, position, label, box }: Props): JSX.Element {
   const [mouseHover, setMouseHover] = React.useState<boolean>(false)
 
+  const handleStyle = {
+    all: {
+      width: '15px',
+      height: '15px',
+      lineHeight: '15px',
+      fontSize: '10px',
+      fontWeight: 'bold',
+      textAlign: 'center' as 'center',
+      color: box.textColor,
+      borderColor: getDarkerColor(box.backgroundColor as string)
+    },
+    'hover-false': {
+      backgroundColor: box.backgroundColor
+    },
+    'hover-true': {
+      backgroundColor: getDarkerColor(box.backgroundColor as string)
+    }
+  }
+
   return (
     <Handle
       id={id}
       type={type}
       position={position}
       style={{
-        width: '15px',
-        height: '15px',
-        borderColor: getDarkerColor(box.backgroundColor as string),
-        backgroundColor: !mouseHover ? box.backgroundColor : getDarkerColor(box.backgroundColor as string)
+        ...handleStyle.all,
+        ...handleStyle[`hover-${mouseHover ? 'true' : 'false'}`]
       }}
       onMouseEnter={() => setMouseHover(true)}
       onMouseLeave={() => setMouseHover(false)}
     >
-      <span
-        style={{
-          fontSize: '10px',
-          fontWeight: 'bold',
-          color: box.textColor,
-          position: 'absolute',
-          top: '60%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)'
-        }}
-      >
-        {label}
-      </span>
+      {label}
     </Handle>
   )
 }
