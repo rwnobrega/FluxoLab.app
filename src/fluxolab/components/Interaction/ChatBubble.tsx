cb4f0514c8761475d102faba31980cdf18b8e762
Roberto Nobrega <rwnobrega@gmail.com>
2022-08-20 12:29:19 -0300

    Slightly adjust chat bubbles

--- src/fluxolab/components/Interaction/ChatBubble.tsx
+++ src/fluxolab/components/Interaction/ChatBubble.tsx
@@ -7,19 +7,28 @@ interface BubbleProps {
   text: string
 }
 
-const spanClasses = {
-  common: 'badge font-monospace fw-normal p-2',
-  in: 'text-bg-primary align-self-center',
-  out: 'text-bg-success'
-}
-
 const divClasses = {
   common: 'd-flex',
   in: 'align-self-end',
   out: 'align-self-start'
 }
 
-const arrowStyle = {
+const divStyles = {
+  in: {
+    transform: 'translateX(8px)'
+  },
+  out: {
+    transform: 'translateX(-8px)'
+  }
+}
+
+const spanClasses = {
+  common: 'badge font-monospace fw-normal p-2',
+  in: 'text-bg-primary',
+  out: 'text-bg-success'
+}
+
+const arrowStyles = {
   common: {
     width: 0,
     height: 0,
@@ -38,10 +47,10 @@ const arrowStyle = {
 
 export default function ({ direction, text }: BubbleProps): JSX.Element {
   return (
-    <div className={`${divClasses.common} ${divClasses[direction]}`}>
-      {direction === 'out' && <div style={{ ...arrowStyle.common, ...arrowStyle.out }} />}
+    <div className={`${divClasses.common} ${divClasses[direction]}`} style={divStyles[direction]}>
+      {direction === 'out' && <div style={{ ...arrowStyles.common, ...arrowStyles.out }} />}
       <span className={`${spanClasses.common} ${spanClasses[direction]}`}>{text}</span>
-      {direction === 'in' && <div style={{ ...arrowStyle.common, ...arrowStyle.in }} />}
+      {direction === 'in' && <div style={{ ...arrowStyles.common, ...arrowStyles.in }} />}
     </div>
   )
 }
