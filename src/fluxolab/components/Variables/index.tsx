56af83af465ca6c3575fbe960fa0d7e4247cb93d
Roberto Nobrega <rwnobrega@gmail.com>
2022-08-21 21:53:15 -0300

    Convert more stuff to react-bootstrap

--- src/fluxolab/components/Variables/index.tsx
+++ src/fluxolab/components/Variables/index.tsx
@@ -2,6 +2,7 @@ import _ from 'lodash'
 
 import React, { useCallback } from 'react'
 
+import Button from 'react-bootstrap/Button'
 import Table from 'react-bootstrap/Table'
 
 import useStoreMachine from 'stores/storeMachine'
@@ -32,24 +33,22 @@ export default function (): JSX.Element {
     <div>
       <div className='d-flex flex-row justify-content-between align-items-center mb-2 gap-3'>
         <span className='fw-semibold'>Variáveis</span>
-        <button
-          type='button'
-          className='btn btn-primary btn-sm fw-semibold text-nowrap'
+        <Button
+          size='sm'
+          className='fw-semibold text-nowrap'
           onClick={() => addVariable(getNextVariableId(), 'num')}
           disabled={state.timeSlot !== 0}
         >
           Adicionar variável
-        </button>
+        </Button>
       </div>
-      <div className='vstack gap-3 overflow-auto'>
-        <Table size='sm' variant='borderless'>
-          <tbody>
-            {_.map(machine.variables, ({ id }, index) => (
-              <VariableItem key={index} id={id} disabled={state.timeSlot !== 0} />
-            ))}
-          </tbody>
-        </Table>
-      </div>
+      <Table size='sm' variant='borderless'>
+        <tbody>
+          {_.map(machine.variables, ({ id }, index) => (
+            <VariableItem key={index} id={id} disabled={state.timeSlot !== 0} />
+          ))}
+        </tbody>
+      </Table>
     </div>
   )
 }
