f70146cdb342d1e1e51197e67a3fe8f3b87a27d2
Roberto Nobrega <rwnobrega@gmail.com>
2022-08-26 22:40:38 -0300

    Remove unimplemented delete and duplicate actions

--- src/fluxolab/components/Navbar/Current.tsx
+++ src/fluxolab/components/Navbar/Current.tsx
@@ -24,36 +24,16 @@ export default function (): JSX.Element {
     }
   }, [])
 
-  const onClickDuplicate = (): void => {
-  }
-
   return (
-    <div className='input-group'>
-      <input
-        type='text'
-        className='form-control bg-dark text-white fs-4'
-        value={flowchartTitle}
-        onFocus={onFocus}
-        onBlur={onBlur}
-        onChange={onChange}
-        onKeyDown={onKeyDown}
-        style={{ borderColor: editMode ? 'white' : 'black' }}
-      />
-      <button type='button' className='btn btn-dark' data-bs-toggle='dropdown'>
-        <i className='bi bi-three-dots-vertical' />
-      </button>
-      <ul className='dropdown-menu dropdown-menu-end'>
-        <li>
-          <button className='dropdown-item' onClick={onClickDuplicate}>
-            Duplicar
-          </button>
-        </li>
-        <li>
-          <button className='dropdown-item'>
-            Deletar
-          </button>
-        </li>
-      </ul>
-    </div>
+    <input
+      type='text'
+      className='form-control bg-dark text-white fs-4'
+      value={flowchartTitle}
+      onFocus={onFocus}
+      onBlur={onBlur}
+      onChange={onChange}
+      onKeyDown={onKeyDown}
+      style={{ borderColor: editMode ? 'white' : 'black' }}
+    />
   )
 }
