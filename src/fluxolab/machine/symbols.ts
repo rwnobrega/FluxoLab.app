5da29a9fad474c922198b3cdc1ec872983bcf751
Roberto Nobrega <rwnobrega@gmail.com>
2023-08-10 10:24:27 -0300

    Create `parser.ts`

--- src/fluxolab/machine/symbols.ts
+++ src/fluxolab/machine/symbols.ts
@@ -1,38 +1,8 @@
+import { Symbol } from 'machine/types'
 import _ from 'lodash'
 
-import evaluate, { registerFunction } from 'ts-expression-evaluator'
-
-import { Symbol } from 'machine/types'
-
+import evaluate from 'machine/parser'
 import { getVariableType } from 'machine/variables'
-
-registerFunction('div', (a: any, b: any) => Math.round(a / b))
-registerFunction('mod', (a: any, b: any) => a % b)
-registerFunction('pow', (a: any, b: any) => Math.pow(a, b))
-registerFunction('sqrt', (a: any) => Math.sqrt(a))
-registerFunction('log', (a: any) => Math.log(a))
-registerFunction('log10', (a: any) => Math.log10(a))
-registerFunction('log2', (a: any) => Math.log2(a))
-registerFunction('exp', (a: any) => Math.exp(a))
-registerFunction('sin', (a: any) => Math.sin(a))
-registerFunction('cos', (a: any) => Math.cos(a))
-registerFunction('tan', (a: any) => Math.tan(a))
-registerFunction('asin', (a: any) => Math.asin(a))
-registerFunction('acos', (a: any) => Math.acos(a))
-registerFunction('atan', (a: any) => Math.atan(a))
-registerFunction('sinh', (a: any) => Math.sinh(a))
-registerFunction('cosh', (a: any) => Math.cosh(a))
-registerFunction('tanh', (a: any) => Math.tanh(a))
-registerFunction('asinh', (a: any) => Math.asinh(a))
-registerFunction('acosh', (a: any) => Math.acosh(a))
-registerFunction('atanh', (a: any) => Math.atanh(a))
-registerFunction('sign', (a: any) => Math.sign(a))
-registerFunction('abs', (a: any) => Math.abs(a))
-registerFunction('round', (a: any) => Math.round(a))
-registerFunction('floor', (a: any) => Math.floor(a))
-registerFunction('ceil', (a: any) => Math.ceil(a))
-registerFunction('min', (a: any, b: any) => Math.min(a, b))
-registerFunction('max', (a: any, b: any) => Math.max(a, b))
 
 export function newStartSymbol (params: { id: string, nextId: string }): Symbol {
   const { id, nextId } = params
