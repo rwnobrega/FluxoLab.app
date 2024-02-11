28802110c5c646a15ec550684d51b25eb55601bf
Roberto Nobrega <rwnobrega@gmail.com>
2024-02-11 00:17:40 -0300

    Improve error messages

--- src/fluxolab/machine/compiler.ts
+++ src/fluxolab/machine/compiler.ts
@@ -48,37 +48,60 @@ export default function compile ({ nodes, edges, variables }: CompilerInput): Co
   function compileFlowchart (): Symbol[] {
     const flowchart: Symbol[] = []
     for (const { id, type, data } of nodes) {
-      if (type === 'start') {
-        const nextId = getOutgoingNode(id, 'out', edges)
-        flowchart.push(newStartSymbol({ id, nextId }))
-      } else if (type === 'assignment') {
-        const split: string[] = _.map(data.value.split('='), _.trim)
-        if (split.length !== 2) {
-          throw new CompileError('Expressão inválida', [id])
+      switch (type) {
+        case 'start': {
+          const nextId = getOutgoingNode(id, 'out', edges)
+          flowchart.push(newStartSymbol({ id, nextId }))
+          break
         }
-        const [variableId, expression] = split
-        if (!_.some(variables, { id: variableId })) {
-          throw new CompileError(`Variável "${variableId}" não existe.`, [id])
+        case 'assignment': {
+          const split: string[] = _.map(data.value.split('='), _.trim)
+          if (split.length !== 2) {
+            throw new CompileError('Expressão inválida', [id])
+          }
+          const [variableId, expression] = split
+          if (variableId === '') {
+            throw new CompileError('Variável não especificada.', [id])
+          }
+          if (!_.some(variables, { id: variableId })) {
+            throw new CompileError(`Variável "${variableId}" não existe.`, [id])
+          }
+          const nextId = getOutgoingNode(id, 'out', edges)
+          flowchart.push(newAssignmentSymbol({ id, variableId, expression, nextId }))
+          break
         }
-        const nextId = getOutgoingNode(id, 'out', edges)
-        flowchart.push(newAssignmentSymbol({ id, variableId, expression, nextId }))
-      } else if (type === 'conditional') {
-        const condition = data.value
-        const nextTrue = getOutgoingNode(id, 'true', edges)
-        const nextFalse = getOutgoingNode(id, 'false', edges)
-        flowchart.push(newConditionalSymbol({ id, condition, nextTrue, nextFalse }))
-      } else if (type === 'input_') {
-        const variableId = data.value
-        if (!_.some(variables, { id: variableId })) {
-          throw new CompileError(`Variável "${variableId as string}" não existe.`, [id])
-        } const nextId = getOutgoingNode(id, 'out', edges)
-        flowchart.push(newInputSymbol({ id, variableId, nextId }))
-      } else if (type === 'output_') {
-        const expression = data.value
-        const nextId = getOutgoingNode(id, 'out', edges)
-        flowchart.push(newOutputSymbol({ id, expression, nextId }))
-      } else if (type === 'end') {
-        flowchart.push(newHaltSymbol({ id }))
+        case 'conditional': {
+          const condition = data.value
+          const nextTrue = getOutgoingNode(id, 'true', edges)
+          const nextFalse = getOutgoingNode(id, 'false', edges)
+          flowchart.push(newConditionalSymbol({ id, condition, nextTrue, nextFalse }))
+          break
+        }
+        case 'input_': {
+          const variableId: string = data.value
+          if (variableId === '') {
+            throw new CompileError('Variável não especificada.', [id])
+          }
+          if (!_.some(variables, { id: variableId })) {
+            throw new CompileError(`Variável "${variableId}" não existe.`, [id])
+          }
+          const nextId = getOutgoingNode(id, 'out', edges)
+          flowchart.push(newInputSymbol({ id, variableId, nextId }))
+          break
+        }
+        case 'output_': {
+          const expression = data.value
+          const nextId = getOutgoingNode(id, 'out', edges)
+          flowchart.push(newOutputSymbol({ id, expression, nextId }))
+          break
+        }
+        case 'end': {
+          flowchart.push(newHaltSymbol({ id }))
+          break
+        }
+        default: {
+          throw new CompileError(`Tipo de nó desconhecido: ${type as string}`, [id])
+        }
       }
     }
     return flowchart
