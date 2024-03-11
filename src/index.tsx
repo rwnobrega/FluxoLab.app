2acf5ee0c2e0cf24408be5eb45ccbc36d967dc4d
Roberto Nobrega <rwnobrega@gmail.com>
2024-03-10 22:57:18 -0300

    Add React Flow CSS import

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from 'components/App'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'reactflow/dist/style.css'
 
 const container = document.createElement('div')
 const root = createRoot(container)
