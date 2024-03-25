473490e0cda77868c835d24c6f518e07a59ca716
Roberto Nobrega <rwnobrega@gmail.com>
2024-03-24 22:11:40 -0300

    Add toast when changing language

--- src/fluxolab/components/Navbar/MenuLanguage/index.tsx
+++ src/fluxolab/components/Navbar/MenuLanguage/index.tsx
@@ -1,20 +1,30 @@
 import _ from 'lodash'
 
-import React from 'react'
+import React, { useCallback } from 'react'
 
 import Dropdown from 'react-bootstrap/Dropdown'
 
 import Tooltip from 'components/General/Tooltip'
 
+import useStoreEphemeral from 'stores/useStoreEphemeral'
 import useStoreStrings from 'stores/useStoreStrings'
 
 export default function (): JSX.Element {
   const { getString, language: currentLanguage, setLanguage } = useStoreStrings()
+  const { setToastContent } = useStoreEphemeral()
 
   const languages: Record<string, string> = {
     en: 'English',
     'pt-BR': 'Português (BR)'
   }
+  const handleClick = useCallback((language: string) => {
+    setLanguage(language)
+    setToastContent({
+      message: getString('ToastMessage_LanguageChanged', { language: languages[language] }),
+      icon: 'bi-check2-circle',
+      background: 'success'
+    })
+  }, [])
 
   return (
     <Dropdown align='end'>
@@ -25,7 +35,7 @@ export default function (): JSX.Element {
       </Tooltip>
       <Dropdown.Menu>
         {_.map(languages, (label, language) => (
-          <Dropdown.Item key={language} onClick={() => setLanguage(language)}>
+          <Dropdown.Item key={language} onClick={() => handleClick(language)}>
             {language === currentLanguage ? <i className='bi bi-check2' /> : null} {label}
           </Dropdown.Item>
         ))}
