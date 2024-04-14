736267a612c4ed207cbaa04fb89bc13ce744d783
Roberto Nobrega <rwnobrega@gmail.com>
2024-04-14 13:10:20 -0300

    Connection end is now in block center

--- src/fluxolab/components/Flow/MyNode/MyHandleTarget.tsx
+++ src/fluxolab/components/Flow/MyNode/MyHandleTarget.tsx
@@ -4,20 +4,12 @@ import { Handle, HandleProps, Position } from "reactflow";
 
 import { BoxStyle } from "components/Blocks";
 
-import { getDarkerColor } from "utils/colors";
-
-import useStoreEphemeral from "stores/useStoreEphemeral";
-
 type Props = HandleProps & {
   boxStyle: BoxStyle;
   label?: string;
 };
 
-export default function ({ id, type, boxStyle }: Props): JSX.Element {
-  const { textColor, backgroundColor } = boxStyle;
-
-  const { isConnectingEdge } = useStoreEphemeral();
-
+export default function ({ id, type }: Props): JSX.Element {
   return (
     <Handle
       id={id}
@@ -26,12 +18,12 @@ export default function ({ id, type, boxStyle }: Props): JSX.Element {
       isConnectableStart={false}
       className="react-flow__handle"
       style={{
-        background: backgroundColor,
-        width: "15px",
-        height: "15px",
-        borderColor: getDarkerColor(backgroundColor as string),
-        color: textColor,
-        opacity: isConnectingEdge ? 1 : 0,
+        width: "80px",
+        height: "40px",
+        top: "0px",
+        borderRadius: "0",
+        backgroundColor: "transparent",
+        borderColor: "transparent",
       }}
     />
   );
