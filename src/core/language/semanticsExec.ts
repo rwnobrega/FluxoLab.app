0bce62486663dc5265c758697d7ac8314c7b66c6
Roberto Nobrega <rwnobrega@gmail.com>
2024-05-04 21:50:43 -0300

    Small changes

--- src/core/language/semanticsExec.ts
+++ src/core/language/semanticsExec.ts
@@ -3,6 +3,7 @@ import * as ohm from "ohm-js";
 
 import { VariableTypeId, getVariableType } from "~/core/variableTypes";
 import { MachineState } from "~/store/useStoreMachine";
+import assert from "~/utils/assert";
 
 export function execStart(a: ohm.Node): void {
   const state: MachineState = this.args.state;
@@ -11,18 +12,19 @@ export function execStart(a: ohm.Node): void {
 
 export function execRead(a: ohm.Node, b: ohm.Node): void {
   const state: MachineState = this.args.state;
+  assert(state.input !== null);
   const variableId = b.sourceString;
-  const input = state.input ?? "";
   const { type } = state.memory[variableId];
   const variableType = getVariableType(type);
-  if (!variableType.stringIsValid(input)) {
+  if (!variableType.stringIsValid(state.input)) {
     throw {
       message: "RuntimeError_InvalidInput",
-      payload: { input, type },
+      payload: { input: state.input, type },
     };
   }
-  state.interaction.push({ direction: "in", text: input });
-  state.memory[variableId].value = variableType.parse(input);
+  state.interaction.push({ direction: "in", text: state.input });
+  state.memory[variableId].value = variableType.parse(state.input);
+  state.input = null;
   state.outPort = "out";
 }
 
